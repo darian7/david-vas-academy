e1a3c0c2618bd9d9b9b277726c3ada00f0df6e4b
darian7 <yoj.cabrera@udla.edu.co>
2021-07-01 18:37:28 -0500

    refactor video saga

--- src/services/Video/VideoSaga.js
+++ src/services/Video/VideoSaga.js
@@ -1,23 +1,23 @@
 import { put, takeLatest, all } from 'redux-saga/effects';
 import Api from '../../common/Api/Api'
-import { temary as temaryActions } from "../Temary/TemaryActions"
 import { video as videoActions } from './VideoActions';
 
 function* videoStart({ payload }) {
-  const response = yield Api.put(`/video/video-start/${payload?.video?.id}`, { courseId: payload.courseId })
+  const response = yield Api.put(`/video/video-start/${payload?.video?.id}`, undefined, undefined)
 
   if (response.ok) {
     yield put(videoActions.videoStartResponse());
-    yield put(temaryActions.getTemaryResponse(response.payload.temarys))
-    payload.callback(true)
+
+    payload.callback(payload?.lesson)
   } else {
     const err = new TypeError(response?.payload?.error || 'ERROR_VIDEO_START')
     yield put(videoActions.videoStartResponse(err))
   }
 }
 
-function* getVideosCategorys({ payload: { categoryId, year } }) {
-  const response = yield Api.get(`/video/get-video-date/${categoryId}/${year}`)
+function* getVideosCategorys({ payload }) {
+  const response = yield Api.get(`/video/get-video-date/${payload?.categoryId}`)
+
   if (response.ok) {
     yield put(videoActions.getVideosCategorysResponse(response.payload));
   } else {
