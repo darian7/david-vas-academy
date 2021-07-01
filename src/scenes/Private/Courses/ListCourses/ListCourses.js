890be98eb00a6ef83a50f99b233216daef9a8c26
darian7 <yoj.cabrera@udla.edu.co>
2021-07-01 11:03:46 -0500

    feat logout

--- src/scenes/Private/Courses/ListCourses/ListCourses.js
+++ src/scenes/Private/Courses/ListCourses/ListCourses.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useDispatch } from 'react-redux'
+import { auth as authActions } from '../../../../services/Auth/AuthActions'
 
-export const ListCourses = () => {
+export const ListCourses = (
+) => {
+
+  const dispatch = useDispatch()
+  const { t } = useTranslation()
+
+  useEffect(() => {
+  }, [])
+
+  const goLogout = () => {
+    dispatch(authActions.logout())
+  }
+
   return (
     <div>
-      <h1 style={{ backgroundColor: "white" }}>
-        courses
-      </h1>
+      <div>
+        <h1 style={{ backgroundColor: "white" }}>
+          courses
+        </h1>
+        <button onClick={goLogout} >
+          {t(`button.logout`)}
+        </button>
+      </div>
     </div>
   )
 }
