b278a29d986068ef772bf6c62c44f13d15c6322e
Kevin <kevingalindos@gmail.com>
2021-07-05 22:56:07 -0500

    feat( ListCourses) view courses, components profileLesson and Modules full stylized

--- src/scenes/Private/Courses/Components/ProfileLesson/ProfileLesson.js
+++ src/scenes/Private/Courses/Components/ProfileLesson/ProfileLesson.js
@@ -1,76 +1,76 @@
-import React, { useState } from 'react'
-import ReactPlayer from 'react-player';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState } from "react";
+import ReactPlayer from "react-player";
+import { useDispatch, useSelector } from "react-redux";
 
-import { video as videoActions } from '../../../../../services/Video/VideoActions';
-import { course as courseActions } from '../../../../../services/Course/CourseActions';
+import { video as videoActions } from "../../../../../services/Video/VideoActions";
+import { course as courseActions } from "../../../../../services/Course/CourseActions";
 
-export const ProfileLesson = ({
-  isLesson,
-  isCourse: course
-}) => {
-  const [isDuration, setDuration] = useState(0)
-  const { loading: loadingCourse } = useSelector((state) => state.course)
-  const { loading } = useSelector((state) => state.video)
-  const { profile } = useSelector((state) => state.user)
-  const dispatch = useDispatch()
+export const ProfileLesson = ({ isLesson, isCourse: course }) => {
+  const [isDuration, setDuration] = useState(0);
+  const { loading: loadingCourse } = useSelector((state) => state.course);
+  const { loading } = useSelector((state) => state.video);
+  const { profile } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
   const handleVideoView = (view) => {
     if (view && isLesson?.video?.users?.length == 0 && !loading.video_start) {
-      startVideo(isLesson)
+      startVideo(isLesson);
     }
-  }
+  };
 
   const startVideo = (lesson) => {
     if (!loading?.video_start) {
-      dispatch(videoActions.videoStart(
-        lesson?.video,
-        course?.id,
-        succesVideoStart,
-        lesson
-      ))
+      dispatch(
+        videoActions.videoStart(
+          lesson?.video,
+          course?.id,
+          succesVideoStart,
+          lesson
+        )
+      );
     }
-  }
+  };
 
   const succesVideoStart = (isLesson) => {
     dispatch(
       courseActions.getCourseResponse({
         ...course,
         lessons: course?.lessons?.map((lesson) =>
-          lesson?.id == isLesson?.id ?
-            { ...lesson, video: { ...lesson?.video, users: [profile] } }
-            :
-            lesson
+          lesson?.id == isLesson?.id
+            ? { ...lesson, video: { ...lesson?.video, users: [profile] } }
+            : lesson
         ),
       })
-    )
-  }
+    );
+  };
 
   return (
-    <div>
-      <div>
-        <h1>{course?.title?.es}</h1>
-        <h1>{course?.subtitle?.es}</h1>
-        <h1></h1>
+    <div className="profile-lesson">
+      <div className="title">
+        <span className="title_box">
+          <h1>{course?.title?.es}</h1>
+          <h2>{course?.subtitle?.es}</h2>
+        </span>
       </div>
 
-      <div>
-        {isLesson?.video?.urlVimeo &&
-          <div>
+      <div className="player-wrapper">
+        {isLesson?.video?.urlVimeo && (
+          <div className="video-p">
             <ReactPlayer
-              width={640}
-              height={360}
+              width={1100}
+              height={680}
               playing={true}
               controls={true}
               url={isLesson?.video?.urlVimeo}
-              onProgress={(e) => handleVideoView(e.playedSeconds >= (isDuration - 2))}
+              onProgress={(e) =>
+                handleVideoView(e.playedSeconds >= isDuration - 2)
+              }
               onDuration={(e) => setDuration(e)}
             />
             {<p>{isLesson?.description?.es}</p>}
           </div>
-        }
+        )}
       </div>
-
     </div>
-  )
-}
+  );
+};
--- src/scenes/Private/Courses/ListCourses/ListCourses.js
+++ src/scenes/Private/Courses/ListCourses/ListCourses.js
@@ -1,60 +1,78 @@
-import React, { useEffect, useState } from 'react'
-import { useTranslation } from 'react-i18next'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useDispatch, useSelector } from "react-redux";
 
-import { auth as authActions } from '../../../../services/Auth/AuthActions'
-import { course as courseActions } from '../../../../services/Course/CourseActions'
-import { Modules } from '../Components/Modules/Modules'
-import { ProfileLesson } from '../Components/ProfileLesson/ProfileLesson'
+import { LogoutOutlined } from "@ant-design/icons";
 
-export const ListCourses = (
-) => {
+import { auth as authActions } from "../../../../services/Auth/AuthActions";
+import { course as courseActions } from "../../../../services/Course/CourseActions";
+import { Modules } from "../Components/Modules/Modules";
+import { ProfileLesson } from "../Components/ProfileLesson/ProfileLesson";
 
-  const dispatch = useDispatch()
-  const { t } = useTranslation()
-  const [isLesson, setLesson] = useState()
-  const [isCourse, setCourse] = useState()
-  const { courses, loading: loadingCourse } = useSelector((state) => state.course)
+import { SocialButtons } from '../../../../components/SocialButtons/SocialButtons'
+
+import logo from "../../../../assets/login/Logo-Trader-Expert.png";
+import logoDV from "../../../../assets/login/Logo-DV.png";
+
+export const ListCourses = () => {
+  const dispatch = useDispatch();
+  const { t } = useTranslation();
+  const [isLesson, setLesson] = useState();
+  const [isCourse, setCourse] = useState();
+  const { courses, loading: loadingCourse } = useSelector(
+    (state) => state.course
+  );
 
   console.log("course:", isCourse);
   console.log("lesson:", isLesson);
 
   useEffect(() => {
-    dispatch(courseActions.getCourses())
-  }, [])
+    dispatch(courseActions.getCourses());
+  }, []);
 
   useEffect(() => {
-    if (courses?.length > 0)
-      setCourse(courses[0])
-
-  }, [courses?.length])
+    if (courses?.length > 0) setCourse(courses[0]);
+  }, [courses?.length]);
 
   useEffect(() => {
-    if (isCourse?.lessons?.length > 0)
-      setLesson(isCourse?.lessons[0])
-
-  }, [isCourse])
+    if (isCourse?.lessons?.length > 0) setLesson(isCourse?.lessons[0]);
+  }, [isCourse]);
 
   const goLogout = () => {
-    dispatch(authActions.logout())
-  }
+    dispatch(authActions.logout());
+  };
 
   return (
-    <div style={{ backgroundColor: "white" }}>
-      <div>
-        <button onClick={goLogout} >
-          {t(`button.logout`)}
-        </button>
-      </div>
-
-      <div>
+    <div className="list-courses">
+      <div className="content-player">
+        <div className="header">
+          <div className="logo">
+            <img src={logo} alt="Trader-Expert" />
+          </div>
+          <button onClick={goLogout}>
+            {t(`button.logout`)} <LogoutOutlined />
+          </button>
+        </div>
         <ProfileLesson isLesson={isLesson} isCourse={isCourse} />
       </div>
 
-      <div>
-        <Modules isLesson={isLesson} isCourse={isCourse} setCourse={setCourse} setLesson={setLesson} />
+      <div className="content-modules">
+        <div className="logo-dv">
+          <img src={logoDV} alt="David-Vas" />
+        </div>
+        <div className="mod-box">
+          <Modules
+            isLesson={isLesson}
+            isCourse={isCourse}
+            setCourse={setCourse}
+            setLesson={setLesson}
+          />
+        </div>
+        <div className="social-media">
+          <SocialButtons/>
+        </div>
+        
       </div>
-
     </div>
-  )
-}
+  );
+};
