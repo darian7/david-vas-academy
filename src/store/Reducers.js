d8c0731ace9b064565e0cef51be1be814410beae
darian7 <yoj.cabrera@udla.edu.co>
2021-07-07 13:33:37 -0500

    refactor

--- src/store/Reducers.js
+++ src/store/Reducers.js
@@ -5,28 +5,14 @@ import { auth } from '../services/Auth/AuthActions'
 import reducerAuth from '../services/Auth/AuthReducer'
 import reducerCourse from '../services/Course/CourseReducer'
 import reducerUser from '../services/User/UserReducer'
-import reducerUserQuestion from '../services/UserQuestion/UserQuestionReducer'
-import reducerTemary from '../services/Temary/TemaryReducer'
 import reducerVideo from '../services/Video/VideoReducer'
-import reducerCategory from '../services/Category/CategoryReducer'
-import reducerQuestion from '../services/Question/QuestionReducer'
-import reducerEvent from '../services/Event/EventReducer'
-import reducerTutorial from '../services/Tutorial/TutorialReducer'
-import toolReducer from '../services/Tool/ToolReducer'
 
 const appReducer = (history) => combineReducers({
   router: connectRouter(history),
   auth: reducerAuth,
   course: reducerCourse,
   user: reducerUser,
-  userQuestion: reducerUserQuestion,
-  temary: reducerTemary,
   video: reducerVideo,
-  category: reducerCategory,
-  question: reducerQuestion,
-  event: reducerEvent,
-  tutorial: reducerTutorial,
-  tool: toolReducer,
 })
 
 const rootReducer = (history) => {
